2a4911c68f450d33377bda8e7c0471bf28394fee
Korbinian Schweiger <korbinian.schweiger@gmail.com>
2025-03-10 21:29:02 +0100

    Adapt CommitSkipHooks integration test to actually use a hook

--- pkg/integration/tests/commit/commit_skip_hooks.go
+++ pkg/integration/tests/commit/commit_skip_hooks.go
@@ -5,14 +5,24 @@ import (
 	. "github.com/jesseduffield/lazygit/pkg/integration/components"
 )
 
+var fileModHook = `#!/bin/bash
+
+if [[ -f test-wip-commit-prefix ]]; then
+  echo "Modified text" > test-wip-commit-prefix
+fi
+`
+
 var CommitSkipHooks = NewIntegrationTest(NewIntegrationTestArgs{
 	Description:  "Commit with skip hook using CommitChangesWithoutHook",
 	ExtraCmdArgs: []string{},
 	Skip:         false,
 	SetupConfig:  func(config *config.AppConfig) {},
 	SetupRepo: func(shell *Shell) {
+		shell.CreateFile(".git/hooks/pre-commit", fileModHook)
+		shell.MakeExecutable(".git/hooks/pre-commit")
+
 		shell.NewBranch("feature/TEST-002")
-		shell.CreateFile("test-wip-commit-prefix", "This is foo bar")
+		shell.CreateFile("test-wip-commit-prefix", "Initial text")
 	},
 	Run: func(t *TestDriver, keys config.KeybindingConfig) {
 		t.Views().Commits().
@@ -27,6 +37,8 @@ var CommitSkipHooks = NewIntegrationTest(NewIntegrationTestArgs{
 			Title(Equals("Commit summary")).
 			Type("foo bar").
 			Confirm()
+
+		t.FileSystem().FileContent("test-wip-commit-prefix", Equals("Initial text"))
 
 		t.Views().Commits().Focus()
 		t.Views().Main().Content(Contains("foo bar"))
